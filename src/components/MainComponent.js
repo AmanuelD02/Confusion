c2f0a2a884214cb56a49aa6d85182fd4448f2009
Amanuel <amanueldebebe1000@gmail.com>
2020-11-14 22:05:15 +0300

    Assigment 2 - aboutus component

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -10,6 +10,7 @@ import { DISHES } from '../shared/dishes';
 import { COMMENTS } from '../shared/comments';
 import { PROMOTIONS } from '../shared/promotions';
 import { LEADERS } from '../shared/leaders';
+import About from './AboutComponent';
 
 
 class Main extends Component {
@@ -42,6 +43,7 @@ class Main extends Component {
     )
 
   }
+  
     
     return (
       <div>
@@ -51,7 +53,8 @@ class Main extends Component {
         <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
         <Route exact path='/contactus' component={Contact} />
         <Route path='/menu/:dishId' component={DishWithId} />
-        <Redirect to="/home" />
+        <Route path='/aboutus' component={() => <About leaders={this.state.leaders} /> } />
+        <Redirect to="/home"  />
       </Switch>
       <Footer />
       </div>
